219e0afd5d36989715c33a1b00cba1b9181760ea
Artur Sulej <artur@fresha.com>
2025-03-29 08:21:35 +0100

    WIP

--- tests/ui/decimal_bit_mask.rs
+++ tests/ui/decimal_bit_mask.rs
@@ -1,24 +1,29 @@
 #![warn(clippy::decimal_bit_mask)]
 #![allow(clippy::no_effect)]
-
+#[rustfmt::skip]
 fn main() {
     let mut x = 0;
+    x & 99;    //~ decimal_bit_mask
+    x | 99;    //~ decimal_bit_mask
+    x ^ 99;    //~ decimal_bit_mask
+    x &= 99;   //~ decimal_bit_mask
+    x |= 99;   //~ decimal_bit_mask
+    x ^= 99;   //~ decimal_bit_mask
+
+    x += 99;
+    x -= 99;
+    x *= 99;
+    x /= 99;
+    x %= 99;
+
+    x & 0b1010;
+    x | 0b1010;
+    x ^ 0b1010;
+    x &= 0b1010;
+    x |= 0b1010;
+    x ^= 0b1010;
+
     let mut y = 0;
-    x & 128; //~ decimal_bit_mask
-    x | 1024; //~ decimal_bit_mask
-    x ^ 1024; //~ decimal_bit_mask
-    x &= 1; //~ decimal_bit_mask
-    x |= 2; //~ decimal_bit_mask
-    x ^= 3; //~ decimal_bit_mask
-
-    x += 3;
-
-    x & 0b1100;
-    x | 0b1100;
-    x ^ 0b1100;
-    x &= 0b1100;
-    x |= 0b1100;
-    x ^= 0b1100;
-
     x ^= y;
+
 }
